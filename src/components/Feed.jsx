ad06768a149c86163564b54822e9a89b1bc7e5fa
Aaditshah11 <aaditshah2006@gmail.com>
2026-03-17 22:30:17 +0530

    feat: Implement user feed functionality with Redux store, Feed component, and UserCard display.

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,35 @@
-function Feed() {
-    return (
-        <div>
-            feed
-        </div>
-    )
-}
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addFeed } from "../utils/feedSlice";
+import { useEffect } from "react";
+import UserCard from "./UserCard";
 
-export default Feed
+const Feed = () => {
+  const feed = useSelector((store) => store.feed);
+  const dispatch = useDispatch();
+
+  const getFeed = async () => {
+    if (feed) return;
+    try {
+      const res = await axios.get(BASE_URL + "/user/feed", {
+        withCredentials: true,
+      });
+      dispatch(addFeed(res?.data?.data));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getFeed();
+  }, []);
+  return (
+    feed && (
+      <div className="flex justify-center my-10">
+        <UserCard user={feed[0]} />
+      </div>
+    )
+  );
+};
+export default Feed;
